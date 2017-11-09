56a430722fc7b76b3f2365eee4d94e6ab62380a6
CodingCat <zhunansjtu@gmail.com>
2017-11-09 14:22:23 -0800

    test

--- sql/core/src/main/scala/org/apache/spark/sql/execution/columnar/InMemoryTableScanExec.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/columnar/InMemoryTableScanExec.scala
@@ -219,9 +219,20 @@ case class InMemoryTableScanExec(
       partitionFilter.initialize(index)
 
       if (cachedBatchIterator.isInstanceOf[CachedColumnarPartitionIterator]) {
-        cachedBatchIterator.asInstanceOf[CachedColumnarPartitionIterator].partitionStats
+        val cachedIter = cachedBatchIterator.asInstanceOf[CachedColumnarPartitionIterator]
+        if (!partitionFilter.eval(cachedIter.partitionStats)) {
+          // scalastyle:off
+          println(s"skipped partition $index")
+          // scalastyle:on
+          Iterator()
+        } else {
+          doFilterCachedBatches(cachedBatchIterator, schema, partitionFilter)
+        }
+      } else {
+        doFilterCachedBatches(cachedBatchIterator, schema, partitionFilter)
       }
 
+      /*
       cachedBatchIterator match {
         case cachedIter: CachedColumnarPartitionIterator
           if !partitionFilter.eval(cachedIter.partitionStats) =>
@@ -232,6 +243,7 @@ case class InMemoryTableScanExec(
         case _ =>
           doFilterCachedBatches(cachedBatchIterator, schema, partitionFilter)
       }
+      */
     }
   }
 
