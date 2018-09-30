93611f859f9b4ec0d5c08d646ee02becbc87f3d9
Nan Zhu <nanzhu@uber.com>
2018-09-29 21:19:18 -0700

    stylistic fix

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/v2/DataSourceV2Strategy.scala
@@ -19,8 +19,8 @@ package org.apache.spark.sql.execution.datasources.v2
 
 import scala.collection.mutable
 
-import org.apache.spark.sql.{Strategy, sources}
-import org.apache.spark.sql.catalyst.expressions.{And, Attribute, AttributeReference, AttributeSet, ExprId, Expression, IsNotNull, IsNull, NamedExpression}
+import org.apache.spark.sql.{sources, Strategy}
+import org.apache.spark.sql.catalyst.expressions.{And, Attribute, AttributeReference, AttributeSet, Expression, ExprId, IsNotNull, IsNull, NamedExpression}
 import org.apache.spark.sql.catalyst.planning.{PhysicalOperation, SelectedField}
 import org.apache.spark.sql.catalyst.plans.logical.{AppendData, LogicalPlan, Repartition}
 import org.apache.spark.sql.execution.{FilterExec, ProjectExec, SparkPlan}
